0918b873edfa3b427296c291725ccd0a372e80db
Geovana <geovana.franco01@gmail.com>
2017-09-20 16:34:32 -0300

    first commit

---
+++ src/helloword/CliImpl.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloword;
+
+import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author geova
+ */
+public class CliImpl extends UnicastRemoteObject implements InterfaceCli{
+
+    //private Registry serv;
+    /**
+     *
+     * @param eee
+     * @throws RemoteException
+     */
+    @Override
+    public void echo(String eee) throws RemoteException{
+        System.out.println("Echo recebido: "+eee);
+    }
+    
+    public CliImpl() throws RemoteException{
+        //serv = servidor;
+        //servidor = (InterfaceServ) refSN.lookup("localhost/InterfaceServ");
+          //  servidor.chamar("Cliente", cliente);
+    }
+    
+}
---
+++ src/helloword/Cliente_HelloWord.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloword;
+
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author geova
+ */
+public class Cliente_HelloWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        InterfaceServ servidor = null;
+        try {
+            // TODO code application logic here
+            
+            Registry refSN = LocateRegistry.getRegistry("localhost", 1099);
+            CliImpl cliente = new CliImpl();
+            servidor = (InterfaceServ) refSN.lookup("RefServidor");
+            servidor.chamar("Cliente", cliente);
+        } catch (RemoteException ex) {
+            Logger.getLogger(Cliente_HelloWord.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NotBoundException ex) {
+            Logger.getLogger(Cliente_HelloWord.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
